1d2c61353a436f15367ca892cc1d83cbbc2c4ab6
arjun gupta <arjunkarangupta100@gmail.com>
2022-09-20 03:44:06 -0400

    Initial push which contains all given material for the challenge

---
+++ script.js
@@ -0,0 +1,28 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+var lettersU = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+var lettersL= []
+for(let letter of lettersU){
+  lettersL.push(letter.toLowerCase());
+}
+console.log(lettersU, lettersL);
+
+function generatePassword(){
+
+}
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
