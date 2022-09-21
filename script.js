3a1a962f46121c94f2a974ed8d4cda37bd2882f4
arjun gupta <arjunkarangupta100@gmail.com>
2022-09-21 02:10:50 -0400

    in this push a function to generate a password was added as well as if functions to intiate prompts

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
+
 var lettersU = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-var lettersL= []
+
+var lettersL= ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+
+
+
+
+
+
 for(let letter of lettersU){
+  
   lettersL.push(letter.toLowerCase());
-}
-console.log(lettersU, lettersL);
-
-function generatePassword(){
 
 }
+//console.log(lettersU, lettersL);
+
 
 
 // Write password to the #password input
 function writePassword() {
+ 
+ 
+  // make the function "generate password"
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -24,5 +36,55 @@ function writePassword() {
 
 
 
+
+
+
+
+
+
+// selfmade function
+
+function generatePassword(){
+//prompts the user for uppercase, lowercase, numeric, and/or special characters 
+let upperCase = confirm('would you like to have uppercase letters in the password?');
+
+if (upperCase === true) {
+  
+  return ('bruh3');
+    
+   }
+   else {
+
+  return('getbruhed');
+
+ } 
+ // var lowerCase = confirm('would you like to have lowercase letters in the password?');
+ 
+
+//return "bruh times 2"
+  
+}
+
+
+
+
+
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+// document.createElement("button");
+// generateBtn.innerHTML
+
+// refers to the clicking of the "writePassword" function
+
+generateBtn.addEventListener("click", writePassword);{
+  
+  
+  // console.log('bruh');
+
+
+
+
+
+}
