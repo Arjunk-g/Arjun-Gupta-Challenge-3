d451b5611eb1864ba5e90ddc64644f62b27786a7
arjun gupta <arjunkarangupta100@gmail.com>
2022-09-21 23:26:56 -0400

    went through code with tutor and added more arrays to store user input as we ll as fixing if statements and creating a working for loop

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // Assignment Code
+var choices = [];
+finalChoice = [];
 
 var generateBtn = document.querySelector("#generate");
 
@@ -6,15 +8,16 @@ var lettersU = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
 
 var lettersL = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-var specialCharacters = [' ', '!', '"', '#', '$', '%', '&', '(', ')', '*', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^']
+var specialCharacters = [' ', '!', '"', '#', '$', '%', '&', '(', ')', '*', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^']
 
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-var arrays = [numbers, specialCharacters, lettersL, lettersU]
+
+// var arrays = [numbers, specialCharacters, lettersL, lettersU]
 
 //for(let letter of lettersU){
-  
-  //lettersL.push(letter.toLowerCase());
+
+//lettersL.push(letter.toLowerCase());
 
 //}
 //console.log(lettersU, lettersL);
@@ -23,8 +26,8 @@ var arrays = [numbers, specialCharacters, lettersL, lettersU]
 
 // Write password to the #password input
 function writePassword() {
- 
- 
+
+
   // make the function "generate password"
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -38,43 +41,59 @@ function writePassword() {
 
 // selfmade function
 
-function generatePassword(){
-  console.log(arrays); }
-  //prompts the user for uppercase, lowercase, numeric, and/or special characters 
-//let addUpperCase = confirm('would you like to have uppercase letters in the password?');
+function generatePassword() {
+  // console.log(arrays);
 
-//let addLowerCase = confirm('would you like to have lowercase letters in the password?');
+//prompts the user for uppercase, lowercase, numeric, and/or special characters 
+var addUpperCase = confirm('would you like to have uppercase letters in the password?');
 
-//let addSpecialCharacters = confirm('would you like to have uppercase letters in the password?');
+var addLowerCase = confirm('would you like to have lowercase letters in the password?');
 
-//let addNumbers = confirm('would you like to have uppercase letters in the password?');
+var addSpecialCharacters = confirm('would you like to have special characters letters in the password?');
 
-//if (addUpperCase === true) {
-  
-  //if(addLowerCase === true) {
+var addNumbers = confirm('would you like to have numbers in the password?');
 
-    //if (addSpecialCharacters === true) {
+var length = prompt('Pick a length between 8 and 128 characters');
 
-    //}
-  
- // else {
-//return (addLowerCase);
 
-  //}
-  
-  //return ('bruh3');
-    
-  // }
-  // else {
 
-  //return('getbruhed');
 
- //} 
- // var lowerCase = confirm('would you like to have lowercase letters in the password?');
- 
 
-//return "bruh times 2"
-  
+if (addUpperCase === true) {
+
+  choices = choices.concat(lettersU);
+}
+
+if (addLowerCase === true) {
+
+  choices = choices.concat(lettersL);
+}
+
+if (addSpecialCharacters === true) {
+
+  choices = choices.concat(specialCharacters);
+}
+
+if (addNumbers === true) {
+
+  choices = choices.concat(numbers);
+}
+
+for(var i=0; i<length; i++){
+  var test = Math.floor(Math.random() * choices.length);
+ var test2 = choices[test];
+  finalChoice.push(test2);
+}
+
+console.log(finalChoice.toString());
+var stringPass = finalChoice.toString();
+
+var finalPass = stringPass.replace(/,/g, "");
+console.log(finalPass);
+
+return finalPass
+
+ }
 
 
 
@@ -85,17 +104,9 @@ function generatePassword(){
 // Add event listener to generate button
 
 
-// document.createElement("button");
-// generateBtn.innerHTML
-
 // refers to the clicking of the "writePassword" function
 
-generateBtn.addEventListener("click", writePassword);{
-  
-  
-   console.log('bruh');
-
-
+generateBtn.addEventListener("click", writePassword); {
 
 
 
